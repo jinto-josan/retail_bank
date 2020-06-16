c115a5d34f6778f2afdbdaaf182231786d8308da
santhosh <santhoshreddy923@gmail.com>
2020-06-16 10:58:11 +0530

    minor changes to requirements and primary key in models

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
+
 class Config(object):
     SECRET_KEY= os.environ.get('SECRET_KEY') or 'adasde23324asdk@'
-    SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(os.path.abspath(os.path.dirname(__file__)),'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data.sqlite')
+
