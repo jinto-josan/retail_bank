bf909198705987786942b647f83773e0f2bcba13
7scythe7 <jintojosan@gmail.com>
2020-06-16 09:58:31 +0530

    adding login pages

--- application/forms.py
+++ application/forms.py
@@ -1,4 +1,13 @@
 '''
 To define all the forms for data input
 '''
-from flask_wtf import FlaskForm,Form
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import InputRequired,Length
+
+
+
+class LoginForm(FlaskForm):
+    login = StringField("Login Id",render_kw={'Placeholder':'Login Id'})
+    password = PasswordField("Password", render_kw={'Placeholder':'Password'})
+    submit = SubmitField("Login")
---
+++ application/static/readme.txt
@@ -0,0 +1,1 @@
+All the static files goes in this folder
